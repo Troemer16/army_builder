f27b250719ba68a8521269a70ccbfb6d3c7ece04
Troemer16 <troemer@mail.greenriver.edu>
2020-03-18 06:55:07 -0700

    Inserted and connected to database

--- army_builder/army_builder/App.xaml.cs
+++ army_builder/army_builder/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +7,38 @@ namespace army_builder
 {
     public partial class App : Application
     {
+        public static string dbPath;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string filePath)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            dbPath = filePath;
+
+            using (var source = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("army_builder.database.army_40k.db"))
+            {
+                using (FileStream destination = File.Create(dbPath))
+                {
+                    try
+                    {
+                        source.CopyTo(destination);
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        Console.Write(e.StackTrace);
+                    }
+
+                }
+            }
         }
 
         protected override void OnStart()
--- army_builder/army_builder/CreatePage.xaml.cs
+++ army_builder/army_builder/CreatePage.xaml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.Data.Sqlite;
 
 namespace army_builder
 {
@@ -15,6 +12,58 @@ namespace army_builder
         public CreatePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Dictionary<int, string> factions = new Dictionary<int, string>();
+
+            SqliteConnectionStringBuilder connStr = new SqliteConnectionStringBuilder();
+            connStr.Add("Data Source", App.dbPath);
+            connStr.Add("Foreign Keys", true);
+
+            using(SqliteConnection conn = new SqliteConnection(connStr.ToString()))
+            {
+                //string query = "CREATE TABLE soup_factions (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL)";
+                
+
+                //using (SqliteCommand cmd = conn.CreateCommand())
+                //{
+                  //  cmd.CommandText = query;
+                    //cmd.ExecuteNonQuery();
+                //}
+
+
+                string query = "SELECT * FROM soup_factions";
+                
+                using (SqliteCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    conn.Open();
+                    using (SqliteDataReader reader = cmd.ExecuteReader())
+                    {
+                        //do
+                        //{
+                        //    factions.Add(reader.GetInt32(0), reader.GetString(1));
+                        //} while (reader.Read());
+
+                        while (reader.Read())
+                        {
+                            factions.Add(reader.GetInt32(0), reader.GetString(1));
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+
+            string label = "";
+            foreach(KeyValuePair<int, string> faction in factions)
+            {
+                label += faction + "\n";
+            }
+            myLabel.Text = label;
         }
     }
 }
